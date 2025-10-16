5520a04308189ad7415113e3d0b7c172d1ef0cb4
arjun-geekywolf <arjun.c@geekywolf.com>
2025-10-16 18:09:43 +0530

    DAY 2 added

---
+++ Dot Net/Assignment-02/PrimeCheck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_02
+{
+    internal class PrimeCheck
+    {
+        public bool  IsPrime(int num)
+        {
+           
+            if (num<2)
+                return false;
+
+            for (int i = 2; i < num; i++) {
+
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Dot Net/Assignment-02/ReverseOfNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_02
+{
+    internal class ReverseOfNumber
+    {
+  
+
+        public void PrintReverse(int num)
+        {
+            Console.WriteLine("\n\n\nReverse of a number\n");
+            int rev = 0;
+            int lastDigit;
+
+            while (num != 0)
+            {
+                lastDigit = num % 10;
+                rev=(rev*10)+lastDigit;
+                num = num / 10;
+            }
+
+            Console.WriteLine(rev);
+        }
+    }
+}
