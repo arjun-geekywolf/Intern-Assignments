607639d17e5d69928461776bc3fecb59d6dcb244
arjun-geekywolf <arjun.c@geekywolf.com>
2025-10-17 18:28:08 +0530

    Day 4 C# assignments added

---
+++ Dot Net/Assignment-04/EmployeeManagement/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using EmployeeManagement;
+
+Employee emp = new Employee("Arjun", 75000, "Permanent");
+emp.DisplayEmployees();
+
+Employee emp1 = new Employee("Arun", 80000, "Contract");
+emp1.DisplayEmployees();
+
+
+Console.WriteLine($"No of employees ={Employee.NumberOfEmployees()}");
+Console.WriteLine($"Next Employee id = {Employee.NextEmployeeId()}");
