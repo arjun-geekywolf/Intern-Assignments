ed58347ad1c6456b6f637a875523fa4e3549f5ac
arjun-geekywolf <arjun.c@geekywolf.com>
2025-10-21 17:21:43 +0530

    DAY 5 C# assignment added

---
+++ Dot Net/Assignment-05/01.Library/Books/Magazine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Books
+{
+    internal class Magazine
+    {
+        public void Info()
+        {
+            Console.WriteLine("Magazine Information");
+        }
+    }
+}
